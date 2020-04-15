618c6e449a2e2455c36c9ac5126f439c8ad88924
Harold Parra <gc.harold.parra@gmail.com>
2020-04-15 21:04:19 +0100

    completed repeatString task

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, repeats) {
+  var repeatedString = "";
 
-}
+  if (repeats >= 0) {
+    for (var counter = 1; counter <= repeats; counter += 1) {
+      repeatedString += string;
+    }
+    return repeatedString;
+  } else {
+    return "ERROR";
+  }
+};
 
-module.exports = repeatString
+module.exports = repeatString;

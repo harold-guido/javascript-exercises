f2fd84dcbfe3db6d0afe97057441765dec991346
Harold Parra <gc.harold.parra@gmail.com>
2020-04-16 01:29:37 +0100

    completed reverseString task

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
-}
-
-module.exports = reverseString
+const reverseString = function (string) {
+  var reversedString = "";
+  for (counter = string.length - 1; counter >= 0; counter--) {
+    reversedString += string[counter];
+  }
+  return reversedString;
+};
+module.exports = reverseString;

72373c20d12c6ac8f9b67b8300085e9be252c085
Harold Parra <gc.harold.parra@gmail.com>
2020-04-16 19:08:08 +0100

    completed tempConversion task

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,14 @@
-const ftoc = function() {
+const ftoc = function (fahrenheit) {
+  var celsius = fahrenheit * (5 / 9) - 32 * (5 / 9);
+  return Math.round(celsius * 10) / 10;
+};
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function (celsius) {
+  var fahrenheit = celsius * (9 / 5) + 32;
+  return Math.round(fahrenheit * 10) / 10;
+};
 
 module.exports = {
   ftoc,
-  ctof
-}
+  ctof,
+};

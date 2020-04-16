d37a1da667875f15cdc93dfc5b5e95e954724cf5
Harold Parra <gc.harold.parra@gmail.com>
2020-04-16 02:56:59 +0100

    modified code for sumAll task to be more efficient

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,28 +1,21 @@
-const sumAll = function (number1, number2) {
-  var lowerNumber = 0,
-    higherNumber = 0,
-    sumOfInbetweenNumbers = 0;
+const sumAll = function (lowerNumber, higherNumber) {
+  var sumOfInbetweenNumbers = 0;
   if (
-    number1 < 0 ||
-    number2 < 0 ||
-    typeof number1 != "number" ||
-    typeof number2 != "number"
-  ) {
-    sumOfInbetweenNumbers = "ERROR";
-  } else if (number1 < number2) {
-    lowerNumber = number1;
-    higherNumber = number2;
+    lowerNumber < 0 ||
+    higherNumber < 0 ||
+    typeof lowerNumber != "number" ||
+    typeof higherNumber != "number"
+  )
+    return "ERROR";
 
-    for (counter = lowerNumber; counter <= higherNumber; counter++) {
-      sumOfInbetweenNumbers += counter;
-    }
-  } else {
-    lowerNumber = number2;
-    higherNumber = number1;
+  if (lowerNumber > higherNumber) {
+    var temp = lowerNumber;
+    lowerNumber = higherNumber;
+    higherNumber = temp;
+  }
 
-    for (counter = lowerNumber; counter <= higherNumber; counter++) {
-      sumOfInbetweenNumbers += counter;
-    }
+  for (counter = lowerNumber; counter <= higherNumber; counter++) {
+    sumOfInbetweenNumbers += counter;
   }
 
   return sumOfInbetweenNumbers;

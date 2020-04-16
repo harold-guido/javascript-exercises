73612921d36bb1f7aaca1a0082e0fadaa9159d69
Harold Parra <gc.harold.parra@gmail.com>
2020-04-16 03:13:51 +0100

    completed leapYears task

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  var isLeapYear = `${
+    year % 400 == 0
+      ? "true"
+      : year % 100 == 0
+      ? ""
+      : year % 4 == 0
+      ? "true"
+      : ""
+  }`;
+  return Boolean(isLeapYear);
+};
 
-}
-
-module.exports = leapYears
+module.exports = leapYears;

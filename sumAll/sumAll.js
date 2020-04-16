fe46ff406309b0042f8d56c3cb02bc5c8254a1f5
Harold Parra <gc.harold.parra@gmail.com>
2020-04-16 02:48:02 +0100

    completed sumAll task

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function (number1, number2) {
+  var lowerNumber = 0,
+    higherNumber = 0,
+    sumOfInbetweenNumbers = 0;
+  if (
+    number1 < 0 ||
+    number2 < 0 ||
+    typeof number1 != "number" ||
+    typeof number2 != "number"
+  ) {
+    sumOfInbetweenNumbers = "ERROR";
+  } else if (number1 < number2) {
+    lowerNumber = number1;
+    higherNumber = number2;
 
-}
+    for (counter = lowerNumber; counter <= higherNumber; counter++) {
+      sumOfInbetweenNumbers += counter;
+    }
+  } else {
+    lowerNumber = number2;
+    higherNumber = number1;
 
-module.exports = sumAll
+    for (counter = lowerNumber; counter <= higherNumber; counter++) {
+      sumOfInbetweenNumbers += counter;
+    }
+  }
+
+  return sumOfInbetweenNumbers;
+};
+
+module.exports = sumAll;

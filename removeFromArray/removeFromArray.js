e207c19825d82d331042bc65c4639acf674cdda7
Harold Parra <gc.harold.parra@gmail.com>
2020-04-16 02:29:26 +0100

    completed removeFromArray task

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  var array = [];
+  array = arguments[0];
 
-}
+  for (firstCounter = 0; firstCounter <= array.length - 1; firstCounter++) {
+    for (
+      secondCounter = 1;
+      secondCounter <= arguments.length - 1;
+      secondCounter++
+    ) {
+      if (array[firstCounter] == arguments[secondCounter]) {
+        array.splice(firstCounter, 1); // remember splice needs a range
+        firstCounter--;
+      }
+    }
+  }
 
-module.exports = removeFromArray
+  return array;
+};
+
+module.exports = removeFromArray;

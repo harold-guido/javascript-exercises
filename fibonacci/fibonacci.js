9d66bbb3a08a47de7d2a518fb2cc964c06b8b16d
Harold Parra <gc.harold.parra@gmail.com>
2020-04-27 16:00:33 +0100

    completed fibonacci task

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
-}
-
-module.exports = fibonacci
+const fibonacci = function (num, prevTerm = 0, currTerm = 1) {
+  n = Number(num);
+  if (n < 0) {
+    return "OOPS";
+  }
+  if (n == 1) {
+    return 1;
+  }
+  if (n == 2) {
+    return prevTerm + currTerm;
+  } else {
+    var nextTerm = prevTerm + currTerm;
+    var nthTerm = fibonacci(n - 1, currTerm, nextTerm);
+  }
+  return nthTerm;
+};
+module.exports = fibonacci;

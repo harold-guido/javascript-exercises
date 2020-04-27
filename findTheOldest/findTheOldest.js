829cdc9cbaac7516556f9fc344cc37a2b90c138f
Harold Parra <gc.harold.parra@gmail.com>
2020-04-27 17:16:53 +0100

    completed finTheOldest task

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-let findTheOldest = function() {
+const getAge = function (person) {
+  const currYear = new Date().getFullYear();
 
-}
+  return person.yearOfBirth === undefined
+    ? 0
+    : person.yearOfDeath === undefined
+    ? currYear - person.yearOfBirth
+    : person.yearOfDeath - person.yearOfBirth;
+};
 
-module.exports = findTheOldest
+const maxAgeCallback = function (person1, person2) {
+  return getAge(person1) > getAge(person2) ? person1 : person2;
+};
+
+let findTheOldest = function (people) {
+  const oldest = people.reduce(maxAgeCallback, {});
+  return oldest;
+};
+
+module.exports = findTheOldest;
